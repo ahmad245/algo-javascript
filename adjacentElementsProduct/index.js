38003bfdf323cb48e43660cb3e2c59a8bf3aee7f
Ahmad AL MASRI <a.almasri@onlineformapro.org>
2020-09-16 16:06:09 +0200

    sequence

--- adjacentElementsProduct/index.js
+++ adjacentElementsProduct/index.js
@@ -1,3 +1,13 @@
-const adjacentElementsProduct = (product = []) => {
+const adjacentElementsProduct = (product = [3, 6, -2, -5, 7, 3]) => {
 
+    let max = 0;
+    for (let i = 1; i < product.length; i++) {
+        if (max < product[i] * product[i - 1]) {
+            max = product[i] * product[i - 1];
+        }
+        if (max < product[i] * product[i - 1]) {
+            max = product[i] * product[i + 1];
+        }
+    }
+    return max;
 }
