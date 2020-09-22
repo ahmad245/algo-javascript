98f94c88d17f2abcf485f3bff23625894835b90c
Ahmad AL MASRI <a.almasri@onlineformapro.org>
2020-09-22 10:01:30 +0200

    continue

--- arrayMaxConsecutiveSum/index.js
+++ arrayMaxConsecutiveSum/index.js
@@ -9,3 +9,17 @@
 // 5 + 1 = 6;
 // 1 + 6 = 7.
 // Thus, the answer is 8
+
+const arrayMaxConsecutiveSum = (inputArray = [2, 3, 5, 1, 6], k = 2) => {
+    let max = 0;
+
+    for (let i = 0; i < inputArray.length; i++) {
+        let sum = 0;
+        for (let j = i; j < k + i; j++) {
+            if (j >= inputArray.length) break;
+            sum += inputArray[j];
+        }
+        max = Math.max(max, sum);
+    }
+    return max;
+}
