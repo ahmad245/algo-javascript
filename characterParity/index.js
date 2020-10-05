3cf161e17d15571110927eee8110bdef7f4b16bd
Ahmad AL MASRI <a.almasri@onlineformapro.org>
2020-10-05 16:04:55 +0200

    cont

--- characterParity/index.js
+++
@@ -1,7 +0,0 @@
-function characterParity(symbol = '') {
-    return isNaN(parseInt(symbol)) ? "not a digit" : (parseInt(symbol) % 2 !== 0 ? "odd" : "even")
-}
-
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
--- circleOfNumbers/index.js
+++
@@ -1,6 +0,0 @@
-function circleOfNumbers(n, firstNumber) {
-    if (firstNumber > n / 2) return firstNumber - (n / 2)
-    return (n / 2) + firstNumber
-}
-
-console.log(circleOfNumbers(10, 2));
